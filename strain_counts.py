329d49f2660ce6a1f87efd1049eba0378ab7d306
vbmedina <vbmedinaa@gmail.com>
2025-05-19 12:37:40 +0100

    Clean counts

--- strain_counts.py
+++ strain_counts.py
@@ -2,27 +2,27 @@ import pandas as pd
 from collections import Counter
 
 # File path to the CSV
-csv_file_path = "/Users/victoriamedina/Thesis_Project/Thesis/CHEMBL_Feb_5copy.csv"
+csv_file_path = "/Users/victoriamedina/Thesis_Project/Thesis/CHEMBL_Feb_5.csv"
 
 data = pd.read_csv(csv_file_path)
 
-# Verify the column "verify_strain" exists
-if "verify_strain" in data.columns:
-    # Count occurrences of each item in the "verify_strain" column
-    strain_counts = Counter(data["verify_strain"].dropna())
+# Verify the column "stand_strain" exists
+if "stand_strain" in data.columns:
+    # Count occurrences of each item in the "stand_strain column
+    strain_counts = Counter(data["stand_strain"].dropna())
     
     # Print the counts
     for strain, count in strain_counts.items():
         print(f"{strain}: {count}")
 else:
-    print("The column 'verify_strain' does not exist in the CSV file.")
+    print("The column 'stand_strain' does not exist in the CSV file.")
     strain_counts = Counter()  # Initialize an empty Counter if the column doesn't exist
 
 # Create a DataFrame from the strain counts
 strain_counts_df = pd.DataFrame(strain_counts.items(), columns=["Strain", "Count"])
 
 # File path for the new CSV
-output_csv_path = "/Users/victoriamedina/Thesis_Project/Thesis/strain_counts_2.csv"
+output_csv_path = "/Users/victoriamedina/Thesis_Project/Thesis/strain_counts_3.csv"
 
 # Save the DataFrame to a new CSV file
 strain_counts_df.to_csv(output_csv_path, index=False)
