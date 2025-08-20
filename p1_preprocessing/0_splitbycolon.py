f7820db61eaa8129e3b47b58bff5fd640960dd2e
vbmedina <vbmedinaa@gmail.com>
2025-08-20 18:02:27 +0100

    updating 0_split colon... with right variable

--- p1_preprocessing/0_splitbycolon.py
+++ p1_preprocessing/0_splitbycolon.py
@@ -10,7 +10,7 @@ new = "/Users/victoriamedina/Thesis_Project/other_f.csv"
 
 # Ensure the source file exists
 with open(path, newline="", encoding="utf-8") as f_in, \
-    open(dst, "w", newline="", encoding="utf-8") as f_out:
+    open(new, "w", newline="", encoding="utf-8") as f_out:
     reader = csv.reader(f_in, delimiter=";", quotechar='"')
     writer = csv.writer(f_out)                               
     writer.writerows(reader)                                
